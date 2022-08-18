48f346398a9baaa89b57cf1da19eb119944674dd
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-08-18 19:17:52 +0900

    Add custom link to admin index

--- base/admin.py
+++ base/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 
 admin.AdminSite.site_header = 'Сайт ИМИ СВФУ'
 admin.AdminSite.index_title = 'Администрирование'
+admin.AdminSite.index_template = 'admin/base/index.html'
