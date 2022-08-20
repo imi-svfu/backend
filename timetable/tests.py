5397f8dcab2c3087ea20a34ed7bf1c146984533d
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-08-20 22:59:49 +0900

    Add tests

--- timetable/tests.py
+++ timetable/tests.py
@@ -1,3 +1,102 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Group, Lecturer, Room
+
+
+class TestGroupViewSet(TestCase):
+    name = "Name"
+    subgroups = 1
+
+    def setUp(self):
+        g = Group()
+        g.name = self.name
+        g.subgroups = self.subgroups
+        g.save()
+
+    def test_group_get_list(self):
+        res = self.client.get('/api/groups/')
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+        data = res.json()
+        self.assertEqual(data, [{
+            'id': 1,
+            'name': self.name,
+            'subgroups': self.subgroups,
+        }])
+
+    def test_group_get_existing(self):
+        res = self.client.get('/api/groups/1/')
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+        data = res.json()
+        self.assertEqual(data, {
+            'id': 1,
+            'name': self.name,
+            'subgroups': self.subgroups,
+        })
+
+    def test_group_get_absent(self):
+        res = self.client.get('/api/groups/2/')
+        self.assertEqual(res.status_code, HTTPStatus.NOT_FOUND)
+
+
+class TestRoomViewSet(TestCase):
+    num = "1"
+
+    def setUp(self):
+        r = Room()
+        r.num = self.num
+        r.save()
+
+    def test_room_get_list(self):
+        res = self.client.get('/api/rooms/')
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+        data = res.json()
+        self.assertEqual(data, [{
+            'id': 1,
+            'num': self.num,
+        }])
+
+    def test_room_get_existing(self):
+        res = self.client.get('/api/rooms/1/')
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+        data = res.json()
+        self.assertEqual(data, {
+            'id': 1,
+            'num': self.num,
+        })
+
+    def test_room_get_absent(self):
+        res = self.client.get('/api/rooms/2/')
+        self.assertEqual(res.status_code, HTTPStatus.NOT_FOUND)
+
+
+class TestLecturerViewSet(TestCase):
+    name = "Name"
+
+    def setUp(self):
+        lecturer = Lecturer()
+        lecturer.name = self.name
+        lecturer.save()
+
+    def test_lecturer_get_list(self):
+        res = self.client.get('/api/lecturers/')
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+        data = res.json()
+        self.assertEqual(data, [{
+            'id': 1,
+            'name': self.name,
+        }])
+
+    def test_lecturer_get_existing(self):
+        res = self.client.get('/api/lecturers/1/')
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+        data = res.json()
+        self.assertEqual(data, {
+            'id': 1,
+            'name': self.name,
+        })
+
+    def test_lecturer_get_absent(self):
+        res = self.client.get('/api/lecturers/2/')
+        self.assertEqual(res.status_code, HTTPStatus.NOT_FOUND)
